e10f6204783a9efedd7d2d8806f7f23facb6c7d8
Vitautas Brazas <vbrazas15@gmail.com>
2023-08-31 22:35:27 +0300

    Set it up properly. Create new command - main app command - app:calculate-comissions.

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Kernel;
+use Millon\PhpRefactoring\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace Millon\PhpRefactoring;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
